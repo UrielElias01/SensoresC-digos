c3514e391dca4a1ee38dbfea544a1ef6cfc16b9f
Ángel Uriel Elías Velázquez <107594036+UrielElias01@users.noreply.github.com>
2023-03-06 13:45:06 -0600

    Add files via upload

---
+++ Sensor4/Tap_Module.ino
@@ -0,0 +1,15 @@
+const int knockSensor = 14;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(knockSensor, INPUT);
+}
+
+void loop() {
+  int sensorValue = digitalRead(knockSensor);
+  if (sensorValue == HIGH) {
+    Serial.println("---------------");
+    Serial.println("Knock detected!");
+    Serial.println("---------------");
+  }
+}
