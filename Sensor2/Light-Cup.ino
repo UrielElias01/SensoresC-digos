d52e8b6ab7a948a4a5b6d883d9581e28cc608141
Ángel Uriel Elías Velázquez <107594036+UrielElias01@users.noreply.github.com>
2023-03-09 00:08:43 -0600

    Update Light-Cup.ino

--- Sensor2/Light-Cup.ino
+++ Sensor2/Light-Cup.ino
@@ -1,12 +1,12 @@
 int brightness = 255;   // variable for LED brightness 0~255
 
 void setup() {
-  pinMode(8, INPUT);  // switch is connected to pin 8
-  pinMode(9, OUTPUT); // LED is connected to pin 9
+  pinMode(4, INPUT);  // switch is connected to pin 8
+  pinMode(2, OUTPUT); // LED is connected to pin 9
 }
 
 void loop() {
-  if (digitalRead(8) == HIGH) {
+  if (digitalRead(4) == HIGH) {
     if (brightness < 255) brightness++; 
 // increase brightness if mercury switch is On
   }
@@ -15,5 +15,5 @@ void loop() {
 // decrease brightness if mercury switch is Off (module tilted)
   }
 
-  digitalWrite(9, brightness); // set LED brightness
+  digitalWrite(2, brightness); // set LED brightness
 }
